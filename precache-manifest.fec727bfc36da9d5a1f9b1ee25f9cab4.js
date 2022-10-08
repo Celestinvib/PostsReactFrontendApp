55dd4b55d7e936e10f38546f7e718a8d95926874
Celestinvib <celestinvib@gmail.com>
2022-10-08 17:15:54 +0200

    deploy v1.0

---
+++ precache-manifest.fec727bfc36da9d5a1f9b1ee25f9cab4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb990b3c0463030c21dc",
+    "url": "/static/css/main.8cb6d426.chunk.css"
+  },
+  {
+    "revision": "cb990b3c0463030c21dc",
+    "url": "/static/js/main.cb990b3c.chunk.js"
+  },
+  {
+    "revision": "7157006c3b5bced75b3f",
+    "url": "/static/js/1.7157006c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c0d40c2259ea1eb29c143a3cf2fdf71d",
+    "url": "/index.html"
+  }
+];
